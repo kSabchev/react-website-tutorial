f1d5e73f8ad3cd5e92de8cee3f5498c55eb8b6a4
kSabchev <kaloyansabchev@gmail.com>
2020-11-01 14:35:30 +0200

    initial setup

---
+++ react-website-yt-1/src/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar"></nav>
+        </div>
+    )
+}
+
+export default Navbar
